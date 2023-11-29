bfdc0e3ce3049ff8490f1b4ba3257980fb76763c
Gokul <146622216+huho-0@users.noreply.github.com>
2023-11-29 17:51:48 +0530

    Update sorting_algorithm.c

--- sorting_algorithm.c
+++ sorting_algorithm.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
 
-void gain(int num[], int);
+
+#define AUTHOR "Gokul B"
+
+void display(int[], int);
+void sort(int[], int);
 
 int main(){
-    int num[] = {1,10,12,3,23,5,7,5,2,9,0};
-    int length = (sizeof(num) / sizeof(num[0]));
-    gain(num, length);
+    int unsorted_list[] = {34,43,41,1,32,4,65,41,26,11,52,13,43,12,1,2,45,64,66,3,2,2,5,6,2,34};
+    int length = sizeof(unsorted_list) / sizeof(unsorted_list[0]);  // Used to find the lenght of unsorted list
+    sort(unsorted_list, length);
     return 0;
 }
 
-void gain(int num[], int length){
-    int numbers[60][1];
- 
-    
-    int r;
-    for (r=0;r<length;r++){
-        printf("%d, ",num[r] );
-        numbers[num[r]][num[r]] = num[r];
-    }
+void sort(int list[], int length){
     int i;
     int j;
-    for(i=0;i<60;i++){
-        for(j=0;j<1;j++){
-            printf("[%d,%d]\n", numbers[i][j]);
+    int new_array[100];
+    int new_array_size = 100; // You need to change this value if the highest number in your list is higher than 100
+    for(i=0;i<new_array_size;i++){ 
+        for(j=0;j<length;j++){ 
+            if(i==list[j]){ // This will confirm that the index number and the number in the list are same when when creating a new array
+                new_array[i] = list[j];
+            }
         }
     }
-    
-    
-    
-
+    display(new_array,new_array_size);
 }
+
+void display(int list[], int length){
+    printf("Sorted List: ");
+    int i;
+    for (i=0;i<length;i++){
+        if(list[i]!=0){ // This will ignore if 0 is found in the new array and take all other numbers.
+            printf("%d,", list[i]); // printing the sorted numbers in ascending order
+        }
+    }   
+}
+
+/*As You notice the the algorithm really take a good amount of time in sorting large about of dataset
+Also it is not functional for repeating number, negative number, complex, irrational number and 0, work only in whole number +ve numbers  */
